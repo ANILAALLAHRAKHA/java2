f0ee20def99468c4829d7a289d73649ddcc30b31
ANILA <160001699+ANILAALLAHRAKHA@users.noreply.github.com>
2024-02-26 13:39:09 +0500

    complete work

---
+++ app.js
@@ -0,0 +1,224 @@
+//  chapter5 start 
+  
+  
+//   var number = 4;
+// number2 = 6;
+// document.write( "sum of" +  number + and + number2)
+
+//     Get the input values
+//     var num1 = 4;
+//      num2 = 6;
+
+//     document.write (num1 + num2);
+// document.write(num1 * num2)  
+// document.write(num1 % num2) 
+// var number;
+// # Subtraction
+// num1 = 10
+// num2 = 5
+// result = num1 - num2
+// document.write(result)
+// // # Multiplication
+// num1 = 10
+// num2 = 5
+// result = num1 * num2
+// document.write( result)
+
+// // # Division
+// num1 = 10
+// num2 = 5
+// result = num1 / num2
+// document.write( result)
+
+// // # Modulus
+// num1 = 10
+// num2 = 3
+// result = num1 % num2
+// document.write(result)
+
+//    // a. Declare a variable
+//    var myVariable;
+
+//    // b. Show the value of the variable in your browser like “Value after variable declaration is: ??”
+//    document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+//    // c. Initialize the variable with some number
+//    myVariable = 5;
+
+//    // d. Show the value of the variable in your browser like “Initial value: 5”
+//    document.write("Initial value: " + myVariable + "<br>");
+
+//    // e. Increment the variable
+//    myVariable++;
+
+//    // f. Show the value of the variable in your browser like “Value after increment is: 6”
+//    document.write("Value after increment is: " + myVariable + "<br>");
+
+//    // g. Add 7 to the variable
+//    myVariable += 7;
+
+//    // h. Show the value of the variable in your browser like “Value after addition is: ??”
+//    document.write("Value after addition is: " + myVariable + "<br>");
+
+//    // i. Decrement the variable
+//    myVariable--;
+
+//    // j. Show the value of the variable in your browser like “Value after decrement is: 12”
+//    document.write("Value after decrement is: " + myVariable + "<br>");
+
+//    // k. Show the remainder after dividing the variable’s value by 3
+//    var remainder = myVariable % 3;
+
+//    // l. Output : “The remainder is : 0”
+//    document.write("The remainder is: " + remainder);
+
+
+// Q5
+// var number = 3;
+
+
+// document.write(number + "x 1 =" + number * 1 +"<br>" + number + "x 2 =" + number * 2 +"<br>"
+//  + number + "x 3 =" + number * 3 +"<br>" + number + "x 4 =" + number * 4 +"<br>"
+//  + number + "x 5 =" + number * 5 +"<br>"+ number + "x 6 =" + number * 6 +"<br>"
+//  + number + "x 7 =" + number * 7 +"<br>"+ number + "x 8 =" + number * 8 +"<br>"
+//  + number + "x 9 =" + number * 9 +"<br>"+ number + "x 10 =" + number * 10 +"<br>")
+// Q8
+// var totalMarks = 500;
+// var marksObtained = 425;
+
+// // Calculate percentage
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// // Display the result in the browser
+// document.write("<h2>Percentage Calculation</h2>");
+// document.write("<p>Total Marks: " + totalMarks + "</p>");
+// document.write("<p>Marks Obtained: " + marksObtained + "</p>");
+// document.write("<p>Percentage: " + percentage.toFixed(2) + "%</p>");
+// Q9
+// document.write("<h2>currancy in pk</h2>");
+// var dollars = 10;
+// riyals = 25;
+
+
+// usd_to_pkr_rate = 104.80;
+// sar_to_pkr_rate = 28;
+
+
+// total_pkr = (dollars * usd_to_pkr_rate + riyals * sar_to_pkr_rate)
+
+// document.write(total_pkr)
+  
+// Q10
+
+//  Initialize the variable
+// number = 10
+// result = ((number + 5) * 10) / 2
+// document.write( result)
+
+// Q11
+// var current_year = 2024;
+// birth_year = 1990;
+// age1 = 2024 - 1990;
+// age2 = age1 - 1;
+// document.write( " <h1>AGE CALCULATION</h1>" + "current_year = 2024" + "<br>" + "birth_year = 1990;" + "<br>" + "your age is" + age2)
+
+// Q13
+// var f = "dark chocolate";
+//  age =15;
+//  maximum_age = 65;
+//   snacks=120;
+
+
+// document.write("<h1>The Lifetime Supply Calculator</h1>" + "<br>" +"favorite_snack:" + f +  "<br>"+
+//  "curant age is:" + age  + "<br>" + "estimated maximum age:" + maximum_age  + "<br>"+
+//   " amount_of_per_day_snacks" + snacks)
+//   chapter5 end 
+//  chapter4 start 
+// Q1
+// var a =10;
+//  b=20;
+//   c =  30;
+// document.write(a + b + c)
+// // Q2
+// var a="anu";
+// am="amu";
+// so="sonia";
+// sa="sania";
+// sab="saba";
+// document.write( a + "<br>" + am + "<br>" + so + "<br>" + sa + "<br>" +sab)
+
+// var #nam ="amj";
+// @ga="ajss";
+// %has="gsdjd"
+//  Q3 A 
+// document.write("<h1>“Rules for naming JS variables”</h1>")
+// Q3 b 
+// var m="$my";
+// _="_1variable";
+
+// document.write("Variable names can only contain" + m +"and"+ _ + "<br>") 
+// // Q3 c 
+//   var nam="name"
+// document.write("Variables must begin with a" + nam+ nam +"or" + nam + "<br>")
+// //  Q3 d 
+// var san="sensetive"
+
+// document.write("Variable names are case" + san + "<br>")
+
+// // Q3 e 
+
+// var key="keywords";
+// document.write('Variable names should not be JS' + key + "<br>")
+// // chapter 4 end 
+// // chapter 6 start 
+// // Q1
+// var a=(10);
+// document.write("<h4>Result:</h4>" + "<br>" +"The value of a is:" + a + "<br>")
+
+// var a2= "11";
+// a="11";
+// document.write("The value of ++a is:" + a2 + "<br>" + "Now the value of a is:" + a  + "<br>")
+
+
+// var a3= "11";
+// aa="12";
+// document.write("The value of a++ is:" + a3 + "<br>" + "Now the value of a is:" + aa + "<br>")
+
+// var a_= "11";
+// a1_="11";
+// document.write("The value of --a is:" + a_ + "<br>" + "Now the value of a is:" + a1_ + "<br>")
+
+// var a2_= "11";
+// a2__="10";
+// document.write("The value of a-- is:" + a2_ + "<br>" + "Now the value of a is:" + a__ + "<br>")
+
+// Q2
+// var a =2;
+// b =1;
+// console.log (--a - --b + ++b + +b)
+// console.log(--a - --b )
+// console.log(--a - --b + ++b)
+// console.log(--a - --b + ++b +b--)
+
+// // Q6
+// // # Take input from the user for subject names
+// subject1 =("maths ")
+// subject2 = ("English ")
+// subject3 = ("urdu")
+
+// // # Total marks for each subject
+// // total_marks = 100
+
+// // # Take obtained marks for the first subject from the user
+// obtained_marks_subject1 = (format("Enter the obtained marks for {} out of {}: ".format(subject1, total_marks)))
+
+// // # Printing out the obtained marks for the first subject
+// print("Obtained marks for {}: {}".format(subject1, obtained_marks_subject1))
+
+
+
+
+
+
+
+
